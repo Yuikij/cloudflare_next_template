4a87e60f0977ccf7d2b5f464bdd30693d24c1bd6
zxj <775718540@qq.com>
2025-06-30 13:57:09 +0800

    Integrate next-intl for internationalization support and remove unused global styles and layout components.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import {useTranslations} from 'next-intl';
 
 export default function Home() {
   const [response, setResponse] = useState<string>('')
@@ -19,6 +20,8 @@ export default function Home() {
       setLoading(false)
     }
   }
+
+  const test = useTranslations('test');
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -41,6 +44,9 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
+          </li>
+          <li className="tracking-[-.01em]">
+            {test('test')}
           </li>
         </ol>
 
